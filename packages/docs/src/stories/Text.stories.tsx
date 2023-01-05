861fad363862873e77ebf30573092d2f7e970f85
Daniel <ldaniel.eng@gmail.com>
2023-01-04 22:29:04 -0300

    Added Text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur animi minus soluta saepe expedita sint esse voluptatibus architecto consequatur aut! Consectetur ut neque repellat provident illum, esse consequuntur voluptatum nulla?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
