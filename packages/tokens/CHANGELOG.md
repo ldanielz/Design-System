81a629750d8e72e0c70a2b8ac6a490271cddf2e0
Daniel <ldaniel.eng@gmail.com>
2023-01-05 22:01:07 -0300

    chore: Add new test colors to token

---
+++ packages/tokens/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @enay-ui/tokens
+
+## 1.1.0
+
+### Minor Changes
+
+- Add new Test Color
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -15,4 +15,6 @@ export const colors = {
   ignite500: '#00875F',
   ignite700: '#015F43',
   ignite900: '#00291D',
+
+  test: '#34AB34',
 } as const
